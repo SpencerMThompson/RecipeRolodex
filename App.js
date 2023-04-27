713cc69d4766b51ac1abdd4763b22ab131eb6fbd
Spencer Thompson <st746866@southeast.edu>
2023-04-27 17:20:40 -0400

    logic

--- App.js
+++ App.js
@@ -9,20 +9,31 @@ import {
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import * as SplashScreen from 'expo-splash-screen';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import * as SplashScreen from "expo-splash-screen";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import * as SQLite from "expo-sqlite";
 import { Button } from "react-native";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
+import { Fragment } from "react/cjs/react.production.min";
 
 SplashScreen.preventAutoHideAsync();
 setTimeout(SplashScreen.hideAsync, 2000);
 const art = require("./assets/icon.png");
-const db = SQLite.openDatabase('recipe.db');
+const db = SQLite.openDatabase("recipe.db");
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center",}}>
-      <Text>Hello World</Text>
+    <View style={styles.home}>
+      <Text style={styles.hometext1}>Find Your Favorite Meals</Text>
+      <Text style={styles.hometext2}>Click the rolodex to get started</Text>
+    </View>
+  );
+}
+
+function AboutScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
     </View>
   );
 }
@@ -30,7 +41,6 @@ function HomeScreen({ navigation }) {
 function AddScreen({ navigation }) {
   const [name, setName] = useState(null);
   const [url, setUrl] = useState(null);
-  // const [forceUpdate, forceUpdateId] = useForceUpdate();
   useEffect(() => {
     db.transaction((tx) => {
       tx.executeSql(
@@ -38,89 +48,115 @@ function AddScreen({ navigation }) {
       );
     });
   }, []);
+
   const add = (name, url) => {
-    if(name === null || name ==="" || url === null || url ===""){
+    if (name === null || name === "" || url === null || url === "") {
       return false;
     }
 
-    db.transaction(
-      (tx) => {
-        tx.executeSql("insert into recipes (dishname, dishurl) values (? , ?)", [name, url]);
-      }
-    );
-  }
+    db.transaction((tx) => {
+      tx.executeSql("insert into recipes (dishname, dishurl) values (? , ?);", [
+        name,
+        url,
+      ], (res) => {console.log("add", res)}, (err) => {console.log(err)});
+    });
+  };
 
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Add Recipe Screen</Text>
-      <TextInput value={name} onChangeText={(newName) => setName(newName)} placeholder="Dish Name"></TextInput>
-      <TextInput value={url} onChangeText={(newUrl) => setUrl(newUrl)} placeholder="Recipe URL" ></TextInput>
-      <Button title="Add Recipe" onPress={add(name, url)}></Button>
-      <Text>{name}{url}</Text>
+      <TextInput
+        value={name}
+        onChangeText={(newName) => setName(newName)}
+        placeholder="Dish Name"
+        required={true}
+      ></TextInput>
+      <TextInput
+        value={url}
+        onChangeText={(newUrl) => setUrl(newUrl)}
+        placeholder="Recipe URL"
+        required={true}
+      ></TextInput>
+      <Button title="Add Recipe" onPress={() => add(name, url)}></Button>
     </View>
   );
 }
 
 function ViewScreen({ navigation }) {
-
-  const [isLoading, setIsLoading] = useState(true);
-
-
-  if (isLoading){
-    return(
-      <View>
-        <Text>Content Loading</Text>
-      </View>
-    )
-  }
-  db.transaction(
-    (tx) => {
-      tx.executeSql("select * from items", [], (_, { rows }) =>
-        console.log(JSON.stringify(rows))
+  const [recipes, setRecipes] = useState([]);
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql("select * from recipes", null, (txObj, resultSet) => {
+        setRecipes(resultSet.rows._array)
+      }
       );
-    },
-    null,
-    forceUpdate
-  );
+    }, null);
+  });
+
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>View Recipes Screen</Text>
+      <ScrollView>
+        {recipes.length > 0 &&
+          recipes.map((recipe) => (
+            <Fragment key={recipe.id}>
+              <Text>
+                {recipe.dishname}
+              </Text>
+              <Text>
+                {recipe.dishurl}
+              </Text>
+              <Button title="Delete" onPress={() => db.transaction((tx)=>{tx.executeSql(`delete from recipes where id = ?;`, [recipe.id]); }, null,)}></Button>
+            </Fragment>
+          ))}
+      </ScrollView>
     </View>
   );
 }
-function getRecipes() {
-  const [recipes, setRecipes] = useState([]);
-
-}
-
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" screenOptions={({navigation}) => ({ headerLeft: () => <Pressable onPress={navigation.toggleDrawer}><Image source={art} style={styles.pic}></Image></Pressable>})}>
-        <Drawer.Screen name="Home" component={HomeScreen}/>
-        <Drawer.Screen name="Add Recipe" component={AddScreen}/>
-        <Drawer.Screen name="View Recipe" component={ViewScreen}/>
+      <Drawer.Navigator
+        initialRouteName="Home"
+        screenOptions={({ navigation }) => ({
+          headerLeft: () => (
+            <Pressable onPress={navigation.toggleDrawer}>
+              <Image source={art} style={styles.pic}></Image>
+            </Pressable>
+          ),
+        })
+      }
+      >
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Add Recipe" component={AddScreen} />
+        <Drawer.Screen name="View Recipe" component={ViewScreen} />
+        <Drawer.Screen name="About" component={AboutScreen}/>
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor: 'red',
-    color: 'red',
-    marginTop: 130,
+  home:{
+    flex: 1,
+    backgroundColor: '#CD8987',
+    paddingTop: '50%',
   },
-  pic:{
+  hometext1:{
+    textAlign: 'center',
+    fontSize: 30,
+    color:'#37392E',
+  },
+  hometext2:{
+    textAlign: 'center',
+    fontFamily: 'Serif',
+    color:'#37392E',
+  },
+  pic: {
     width: 40,
     height: 40,
   },
-  tab:{
-    backgroundColor: 'red',
-  }
-
 });
 export default App;
